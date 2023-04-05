e334b027f8d2eea91f8cba766988eb1d5cdb51da
Jonathan Fierro <j@Jonathans-MacBook-Pro.local>
2023-04-05 10:17:00 -0400

    Release 1.6.1

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 'use strict';
 
-require('wrapper-path').init({pathRoot: __dirname, prefix: '$LE'});
+require('wrapper-path').init({pathRoot: __dirname, prefix: '$LE', inGlobal: true});
+let _ = require('lodash');
 
-module.exports = {
-    assignNotRewrite: $LEPath.include('/lib', 'assignNotRewrite.js'),
-    existInArray: $LEPath.include('/lib', 'existInArray.js'),
-    existOneInArray: $LEPath.include('/lib', 'existOneInArray.js'),
-    formatStringWithDictionary: $LEPath.include('/lib', 'formatStringWithDictionary.js'),
-    format: $LEPath.include('/lib', 'formatStringWithDictionary.js'),
-    isArrayObjectWithProp: $LEPath.include('/lib', 'isArrayObjectWithProp.js'),
-    isFloat: $LEPath.include('/lib', 'isFloat.js'),
-    isStringNotEmpty: $LEPath.include('/lib', 'isStringNotEmpty.js'),
-    isUndefinedOrNull: $LEPath.include('/lib', 'isUndefinedOrNull.js'),
-    isUndefinedSet: $LEPath.include('/lib', 'isUndefinedSet.js'),
-    mergeWithArrayConcat: $LEPath.include('/lib', 'mergeWithArrayConcat.js'),
-    paginate: $LEPath.include('/lib', 'paginate.js'),
-    propsMatchFunction: $LEPath.include('/lib', 'propsMatchFunction.js'),
-    randomColor: $LEPath.include('/lib', 'randomColor.js'),
-    randomString: $LEPath.include('/lib', 'randomString.js'),
-    toBoolean: $LEPath.include('/lib', 'toBoolean.js'),
-    toNumberIsEmptyOrUndefinedToNull: $LEPath.include('/lib', 'toNumberIsEmptyOrUndefinedToNull.js'),
-    toNumberIsEmptyOrUndefinedToUndefined: $LEPath.include('/lib', 'toNumberIsEmptyOrUndefinedToUndefined.js'),
-    toNumberIsNaNToNull: $LEPath.include('/lib', 'toNumberIsNaNToNull.js'),
-    toNumberIsNaNToZero: $LEPath.include('/lib', 'toNumberIsNaNToZero.js'),
-    toPercentage: $LEPath.include('/lib', 'toPercentage.js'),
-    uuid: $LEPath.include('/lib', 'uuid.js'),
-    validPercentage: $LEPath.include('/lib', 'validPercentage.js'),
-};
+module.exports = (() => {
+    let libs = [
+        'assignNotRewrite', 'existInArray', 'existOneInArray',
+        'formatStringWithDictionary', 'isArrayObjectWithProp',
+        'isFloat', 'isStringNotEmpty', 'isUndefinedOrNull',
+        'isUndefinedSet', 'mergeWithArrayConcat', 'paginate',
+        'propsMatchFunction', 'randomColor', 'randomString',
+        'toBoolean', 'toNumberIsEmptyOrUndefinedToNull',
+        'toNumberIsEmptyOrUndefinedToUndefined', 'toNumberIsNaNToNull',
+        'toNumberIsNaNToZero', 'toPercentage', 'uuid', 'validPercentage',
+        'toStringNotEmpty', 'cleanObject', 'keyByIndex',
+    ];
+    _.each(libs, (lib) => {
+        if (_.isUndefined(_[lib]))
+            _[lib] = $LEPath.include('/lib', lib + '.js');
+    });
+
+    _['format'] = $LEPath.include('/lib', 'formatStringWithDictionary.js');
+    _['props'] = $LEPath.include('/lib', 'propsMatchFunction.js');
+
+    return _;
+})();
